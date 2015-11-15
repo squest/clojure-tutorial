e988985160085c2cfd355026547e36a1217ef09e
squest <sabdaps@gmail.com>
2015-11-15 07:28:26 +0700

    changed users into atom

--- proone/dev/user.clj
+++ proone/dev/user.clj
@@ -1,6 +1,10 @@
 (ns user
   (:require [clojure.string :as cs]))
 
+(def users
+  (atom ["skadinyo" "0dsapoetra" "beleje" "calvin91" "awol" "leledumbo" "wisnuops" "dizqar"
+         "pelaut" "neilt" "azrax" "codxse" "gilangardya" "dhiamonkey"]))
+
 (defn progress [nama]
   (let [res (-> (slurp (str "http://www.4clojure.com/user/" nama))
                 (cs/split #"/156")
@@ -8,7 +12,7 @@
     (subs res (- (count res) 2))))
 
 (defn all []
-  (doseq [nama ["skadinyo" "0dsapoetra" "beleje" "calvin91" "wisnuops" "dizqar" "pelaut" "neilt" "azrax" "codxse" "gilangardya" "dhiamonkey"]]
+  (doseq [nama @users]
     (println (str "Ucil : " nama))
     (println (str "Progress : " (progress nama)))
     (println "---------------------")))
